9deceec84b5db39a201ff5f6bf641c8046d5856f
matpolsl <mateber553@student.polsl.pl>
2022-05-10 20:57:00 +0200

    Initial Commit

---
+++ SmogApp/SmogAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  SmogAppApp.swift
+//  SmogApp
+//
+//  Created by Mateusz Bereta on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmogAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
